a1e59cab7edf69e3f7787f053e39fcce2cc4d195
Cristian Bonanno <cristian.bonanno@kiratech.it>
2024-12-23 09:37:33 +0100

    feat: replaced PrettyTable with pandas
    Signed-off-by: Cristian Bonanno <cristian.bonanno@kiratech.it>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+import pandas as pd
 from kubernetes import client, config
-from prettytable import PrettyTable
+from tabulate import tabulate
 
 from utils.utils import list_cm, list_ns, list_secret, usage
 
@@ -9,10 +10,8 @@ if __name__ == "__main__":
 
     selected_ns = list_ns(api).items
 
-    table = PrettyTable(
-        ['Namespace', 'Kind', 'Name', 'UsedCount', 'UsedAs', 'UsedBy'])
-    table.align = "l"
-    table.sortby = "UsedCount"
+    df = pd.DataFrame(
+        columns=['Namespace', 'Kind', 'Name', 'UsedCount', 'UsedAs', 'UsedBy'])
 
     for ns in selected_ns:
         ns_name = ns.metadata.name
@@ -21,12 +20,38 @@ if __name__ == "__main__":
         for cm in cm_list.items:
             cm_name = cm.metadata.name
             count, used_as, used_by = usage(api, ns_name, "config_map", cm_name)
-            table.add_row(
-                [ns_name, 'config-map', cm_name, count, used_as, used_by])
+            df.loc[len(df)] = {
+                "Namespace": ns_name,
+                "Kind": "config-map",
+                "Name": cm_name,
+                "UsedCount": count,
+                "UsedAs": used_as,
+                "UsedBy": used_by
+            }
         for secret in secret_list.items:
             secret_name = secret.metadata.name
             count, used_as, used_by = usage(api, ns_name, "secret", secret_name)
-            table.add_row(
-                [ns_name, 'secret', secret_name, count, used_as, used_by])
+            df.loc[len(df)] = {
+                "Namespace": ns_name,
+                "Kind": "secret",
+                "Name": secret_name,
+                "UsedCount": count,
+                "UsedAs": used_as,
+                "UsedBy": used_by
+            }
 
-    print(table)
+    selected_ns_name = []
+
+    if selected_ns_name:
+        print(
+            tabulate(df[df['Namespace'].isin(selected_ns_name)].sort_values(
+                'UsedCount'),
+                     headers='keys',
+                     tablefmt='psql',
+                     showindex=False))
+    else:
+        print(
+            tabulate(df.sort_values('UsedCount'),
+                     headers='keys',
+                     tablefmt='psql',
+                     showindex=False))
